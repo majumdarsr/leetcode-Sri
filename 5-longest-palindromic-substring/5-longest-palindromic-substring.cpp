1a836298d9b77ab0914828e27d915514c99ce536
Sriparna Majumdar <37515344+majumdarsr@users.noreply.github.com>
2022-01-27 21:52:51 -0800

    Time: 256 ms (38.14%), Space: 231.5 MB (22.11%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int start=0; int end=0;
-        if(s.size()==0)
-            return "";
+        if(s.size()<=1)
+            return s;
         else
         {
             for (int center=0;center<s.size();center++)
             {
                 int len1=PaliLength(center, center, s);
                 int len2=PaliLength(center, center+1, s);
-                if(len1>end-start)
+                
+                if(len1 > end - start + 1)
                 {
                   start=center-len1/2;
                     end=center+len1/2;
                 }
-                if(len2>end-start)
+                if(len2 > end - start + 1)
                 {
                     start=center+1-len2/2;
                     end=center+len2/2;
